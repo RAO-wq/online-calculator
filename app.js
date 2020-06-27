4169ea086fe731a9249e000cd47d69dd592a8132
RAOABDULRAHEEM <raotutorial000@gmail.com>
2020-06-27 20:39:01 +0500

    ONLINE CALCUTOR BY RAO

---
+++ app.js
@@ -0,0 +1,22 @@
+function getNumber(num){
+  
+    var output = document.getElementById("output");
+   
+    output.value += num;
+    
+}
+
+function clearResult(){
+
+    var output = document.getElementById("output");
+   
+    output.value = ""
+}
+function getResult(){
+
+    var output = document.getElementById("output");
+  
+    output.value = eval(output.value)
+
+    
+}
